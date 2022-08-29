d2a92e2409a8e2f1caf2b4556aa858a81459a0f9
king rwagatare <kingrwagatare@gmail.com>
2022-08-29 08:52:38 +0200

    set controller

---
+++ app/Http/Controllers/SponsorReview/sponsorReviewController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\SponsorReview;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class sponsorReviewController extends Controller
+{
+    //
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::POST('save_category',[\App\Http\Controllers\Category\categoryController::class,'save_category']);
+Route::POST('register_cooperative',[\App\Http\Controllers\Cooperative\cooperativeController::class,'register_cooperative']);
